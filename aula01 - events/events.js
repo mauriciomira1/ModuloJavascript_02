b00a183adc9857db3bff2cf43f7cb90d77ab0ac7
Maurício Miranda Gomes <121832237+mauriciomira1@users.noreply.github.com>
2023-01-30 16:56:42 -0300

    add

--- aula01 - events/events.js
+++ aula01 - events/events.js
@@ -1,7 +1,12 @@
-function onMouseEnter () {
 
+
+function onMouseEnter () {
+  document.addEventListener('você entrou')
 }
 
 function onClick () {
-
+  let par = document.getElementsByClassName('paragrafo')[0]
+  document.write(par.innerHTML)
 }
+
+
