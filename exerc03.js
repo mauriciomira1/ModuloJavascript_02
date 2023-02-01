02eb006adbf25a6d07421e108ba0feab72a10379
mauriciomira1 <121832237+mauriciomira1@users.noreply.github.com>
2023-01-31 22:39:05 -0300

    mais aulas

---
+++ exerc03.js
@@ -0,0 +1,56 @@
+
+
+function addContact() {
+  const contactSection = document.getElementById('contacts-list')
+  const h3 = document.createElement('h3')
+  h3.className = 'name'
+  h3.innerText = 'Contato'
+  
+  const ul = document.createElement('ul')
+  const nameLi = document.createElement('li')
+  nameLi.innerText = 'Nome: '
+  const nameInput = document.createElement('input')
+  nameInput.type = 'text'
+  nameInput.placeholder = 'Nome do contato'
+  ul.appendChild(nameLi)
+  nameLi.appendChild(nameInput)
+  ul.appendChild(document.createElement('br'))
+
+  const phoneLi = document.createElement('li')
+  phoneLi.innerText = 'Telefone: '
+  const phoneInput = document.createElement('input')
+  phoneInput.type = 'number'
+  phoneInput.placeholder = '(XX)XXXXX-XXXX'
+  ul.appendChild(phoneLi)
+  phoneLi.appendChild(phoneInput)
+  ul.appendChild(document.createElement('br'))
+
+  const addressLi = document.createElement('li')
+  addressLi.innerText = 'Endereço: '
+  const addressInput = document.createElement('input')
+  addressInput.type = 'text'
+  addressInput.placeholder = 'Endereço do contato'
+  addressLi.appendChild(addressInput)
+  ul.appendChild(addressLi)
+  ul.appendChild(document.createElement('br'))
+
+  contactSection.append(h3, ul)
+
+
+
+
+
+
+}
+
+function removeContact () {
+  const contactSection = document.getElementById('contacts-list')
+  const titles = document.getElementsByTagName('h3')
+  const contacts = document.getElementsByTagName('ul')
+
+  contactSection.removeChild(titles[titles.length - 1])
+  contactSection.removeChild(contacts[contacts.length - 1])
+
+ 
+
+}
