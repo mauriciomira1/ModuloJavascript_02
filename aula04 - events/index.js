805875a4e1d1b0502ada968a6d789edafdcaf75d
mauriciomira1 <121832237+mauriciomira1@users.noreply.github.com>
2023-02-02 22:08:36 -0300

    mais aulas

--- aula04 - events/index.js
+++ aula04 - events/index.js
@@ -1,9 +1,22 @@
-const button = document.getElementById('register-button')
-
-button.addEventListener('click', function() {
-  alert('Botão clicado')
-})
-
-function removeEvent () {
-
+function register(ev) {
+  console.log(ev);
+  const username = ev.currentTarget.parentNode.children.username.value;
+  const password = ev.currentTarget.parentNode.children.password.value;
+  const passwordConfirmation =
+    ev.currentTarget.parentNode.children.passwordConfirmation.value;
+  if (password === passwordConfirmation) {
+    alert("Usuário adicionado!");
+  } else alert("As senhas não coincidem!");
 }
+
+const button = document.getElementById("register-button");
+
+button.addEventListener("click", register);
+
+function removeListener() {
+  button.removeEventListener("click", register);
+}
+
+button.addEventListener("mouseover", function (ev) {
+  console.log(ev.currentTarget);
+});
