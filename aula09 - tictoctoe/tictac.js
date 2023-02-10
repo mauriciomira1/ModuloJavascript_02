5d401a829e2afa232e37d2b1ccdd57af0ec7d6bf
mauriciomira1 <121832237+mauriciomira1@users.noreply.github.com>
2023-02-09 23:46:42 -0300

    tittoc continued

--- aula09 - tictoctoe/tictac.js
+++ aula09 - tictoctoe/tictac.js
@@ -1,44 +1,57 @@
 const btnStart = document.getElementById('btnStart')
 let turnPlayerName = ''
+const arrayP1 = []
+const arrayP2 = []
 btnStart.addEventListener('click', function () {
   document.getElementById('players-container').classList = 'hidden'
   document.getElementById('game-container').classList = ''
-  let player1 = document.getElementById('player1').value
-  let player2 = document.getElementById('player2').value
+  const player1 = document.getElementById('player1').value
+  const player2 = document.getElementById('player2').value
   document.getElementById('listPlayers').innerText = player1 + '  VS  ' + player2
-  turnPlayerName = document.getElementById('turnPlayerName')
+  turnPlayerName = document.querySelector('.turnPlayerName')
   turnPlayerName.innerText = "It's " + player1 + "'s turn"
 })
 
 
-  
-let cellClick = document.querySelectorAll('.boardOption').forEach(function (opt) {
-  
-  function test () {
-    if (turnPlayerName.innerText ==  ("It's " + player1 + "'s turn")) {
-      const p1 = opt.addEventListener('click', function () {
-        if (opt.innerText == '') {
-          opt.innerText = 'X'  
-        } else {
-          // Aciona função de a célula piscar em vermelho
-        }
-        turnPlayerName.innerText = ("It's " + player2 + "'s turn")
-      })
+
+document.querySelectorAll('.boardOption').forEach(function (opt) {
+  opt.addEventListener('click', function () {
+    if (turnPlayerName.id == 'p1') {
+      arrayP1.push(opt.id)
+      
+      if (opt.innerText == '') {
+        opt.innerText = 'X'
+      } else {
+        opt.animate(
+          [
+            { color: '#431236', offset: 0.3 }
+          ], {
+            duration: 200,
+            iterations: 3
+          }
+        )
+      }
+      
+      turnPlayerName.innerText = "It's " + player2.value + "'s turn"
+      turnPlayerName.id = 'p2'
     } else {
-      const p2 = opt.addEventListener('click', function () {
+        arrayP2.push(opt.id)
+      
         if (opt.innerText == '') {
-          opt.innerText = 'O'  
+          opt.innerText = 'O'
         } else {
-          // Aciona função de a célula piscar em vermelho
+          opt.animate(
+            [
+              { color: '#431236', offset: 0.3 }
+            ], {
+              duration: 200,
+              iterations: 3
+            }
+          )
         }
-        turnPlayerName.innerText = "It's " + player1 + "'s turn"
-      })
+
+        turnPlayerName.innerText = "It's " + player1.value + "'s turn"
+        turnPlayerName.id = 'p1'
     }
-  }
+  })
 })
-
-cellClick.addEventListener('click', test())
-
-/* function clickedFnct (ev) {
-  
-} */
