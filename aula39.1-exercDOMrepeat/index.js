6bb83ce40d04926bb0dd36e468a2f129e12ff3ca
mauriciomira1 <121832237+mauriciomira1@users.noreply.github.com>
2023-03-14 22:34:18 -0300

    finalizando aula

--- aula39.1-exercDOMrepeat/index.js
+++ aula39.1-exercDOMrepeat/index.js
@@ -7,4 +7,24 @@ const el = new Component ('h1', 'main', { innerText: 'Olá, mundo!' })
 console.log(el)
 el.build().render()
 
-const inp = new Input('body', {innerText: 'Clique aqui!'})
+const form = new Form('body')
+const label = new Label('Nome:', form, { htmlFor: 'inputName'})
+
+
+const input = new Input(form, { placeholder: 'Your name', id: 'inputName' })
+
+form.render()
+
+
+form.addChildren(label, input)
+form.addChildren(
+  new Component ('br'),
+  new Label('Phone:', form, { htmlFor: 'inputNumber'}),
+  new Input(form, {id: 'inputNumber', placeholder: 'Your phone number', type: 'tel'}),
+  new Component ('br'),
+  new Label('Birth:', form, { htmlFor: 'inputBirth'}),
+  new Input(form, {id: 'inputBirth', placeholder: 'Your birth', type: 'date'})
+)
+
+
+
